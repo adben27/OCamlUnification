797fd819fedb91e2e680e395b180298ce4844dc5
adel.bennouar <adel.bennouar@edu.univ-paris13.fr>
2023-04-22 16:23:16 +0200

    Premiere ebauche, avec commentaires

---
+++ unif_antiunif.ml
@@ -0,0 +1,21 @@
+
+type po  = U | V | W | X | Y | A | B | F of po | G of po * po | H of po * po * po (* Termes du premier ordre : d'abord U,V,W,X,Y des variables, puis les fonctions *)
+
+let equal_first t1 t2 = (* Fonction utilie pour anti-unif, pour savoir si on a la meme fonction (donc mm nombre d'args) pour recursiver *) 
+        match (t1, t2) with
+        | (F _, F _) -> true
+        | (G _, G _) -> true
+        | (H _, H _) -> true
+        | (_, _) -> t1 = t2
+
+
+let arite t = match t with 
+        | F(f) -> 1
+        | G(g1,g2) -> 2
+        | H(h1,h2,h3) -> 3
+        | A | B -> 0
+        | _ -> -1;; (* Arité des variables défini à -1, pour avoir un match exhaustif *)
+
+
+
+
