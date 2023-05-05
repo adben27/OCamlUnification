034e9ab078ec4147c55d2aac8fe164768a3a9f42
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2023-05-05 22:56:49 +0200

    correction swap

--- unif_antiunif.ml
+++ unif_antiunif.ml
@@ -1,7 +1,6 @@
-open List
-
 type po  = U | V | W | X | Y | A | B | F of po | G of po * po | H of po * po * po (* Termes du premier ordre : d'abord U,V,W,X,Y des variables, puis les fonctions *)
 exception Echec of string                                                                   
+
 let cons t1 t2 l= (t1, t2)::l;;
 
 let equal_first t1 t2 = (* Fonction utilie pour anti-unif, pour savoir si on a la meme fonction (donc mm nombre d'args) pour recursiver *) 
@@ -18,16 +17,13 @@ let arite t = match t with
   | A | B -> 0
   | _ -> -1;; (* Arité des variables défini à -1, pour avoir un match exhaustif *)
 
-(*Les systeme d'equation seront représentés par une liste de coupe (po,po)*)
-(*e est l'expression (le couple (po,po)) à supprimer et acc est la liste qui est renvoyé sans l'expression e*)
+(*Les systeme d'equation seront représentés par une liste de couple (po,po)*)
+(*e est l'expression (le couple (po,po)) à supprimer*)
 let rec sup e = function
   |[] -> []
   |t::q -> if(t=e) then sup e q else t::(sup e q);;
 
 (*Swap les elements d'un couple*)
-let swap e = let (e1, e2)=e in (e2, e1);;
-
-(*Applique la fonction f à chaque élement égaux à p*)
-let rec map f p = function
+let rec swap e = function 
   |[] -> []
-  |t::q -> if(t=p) then (f t)::(map f p q) else t::(map f p q)
+  |t::q -> if (t=e) then let (e1, e2)=e in ((e2, e1)::(swap e q)) else (t::(swap e q));;
