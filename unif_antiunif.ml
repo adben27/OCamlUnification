b3a831c636310f48b1c00398b74b0e288ce22246
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2023-05-08 16:29:48 +0200

    ajout de la fonction sub_l (substitution liste)

--- unif_antiunif.ml
+++ unif_antiunif.ml
@@ -25,9 +25,15 @@ let arite t =
 (*Cherche si dans la liste de couple l l'élément x apparait si oui on renvoie son associer sinon on leve une exception*)
 (*Exemple: getAsso 1 [(2,4);(3,0);(1,2)] renvoie 2*)
 (*Utile pour savoir quelle substitution faire*)
-let rec getAsso x l = assoc x l
+let rec getAsso (x : po) (l : (po * po) t option) =
+  match l with
+  |None -> raise Not_found
+  |Some l -> match l with
+             |[] -> raise Not_found
+             |t::q -> let (t1,t2)=t in
+                      if(t1=x) then t2 else getAsso x (Some q)
 
-(*Exemple: unif (Var "x") (Func("f", [(Var "x");(Var "y");(Var "z")]) va renvoyez 
+(*Exemple: unif (Var "x") (Func("f", [Var "x"; Var "y"; Var "z"]) va renvoyez 
   la liste de substitution [(Var "x", (Func("f", [Var "x"; Var "y"; Var "z"])))]
   qui veut dire qu'on remplace x par f(x,y,z)*)
 let rec list_subs t1 t2 =
@@ -37,7 +43,7 @@ let rec list_subs t1 t2 =
   |(Var x, _) -> Some [(Var x, t2)]
   |(_, Var y) when (apparait y t1) -> raise (Echec "non unifiable")
   |(_, Var y) -> Some [(Var y, t1)]
-  (*|(Func(f, arg1), Func(g, arg2) -> A faire*)
+  (*|(Func(f, arg1), Func(g, arg2)) -> *)
   |_ -> raise (Echec "non unifiable")
 
 (*Swap les elements de la liste (contient les substitutions que l'on doit faire) si besoin*)
@@ -51,7 +57,26 @@ let rec swap = function
 (*Supprime les couples (Var x, Var x) dans la liste (contient les substitutions que l'on doit faire)*)
 let rec sup = function
   |[] -> []
-  |t::q -> match t with
+  |t::q -> 
+          begin
+           match t with
            |(Var x, Func(f, arg)) -> t::(sup q)
            |(Var x, Var y) -> if(x=y) then sup q else t::(sup q)
            |(_,_) -> t::(sup q)
+          end
+
+(*Substitue tout les termes t1 de subl par t2 dans la liste l*)
+(*Exemple: sub_l [Func("f", [Var "y"; Var "x"]); Func("f", [Var "x"; Var "y"])] (Some [(Var "y", Var "A");(Var "x", Var "B")]);;*)
+(*va renvoyez (::) (Func ("f", [Var "A"; Var "B"]), [Func ("f", [Var "B"; Var "A"])])*)
+let rec sub_l (l : po t) (subl : (po*po) t option)=
+  match subl with
+  |None | Some [] -> l
+  |Some (t::q) -> begin
+                    match l with
+                    |[] -> []
+                    |h::q ->begin 
+                              match h with
+                              |Var x -> (getAsso h subl)::(sub_l q subl)
+                              |Func(f, arg) -> (Func(f, sub_l arg subl))::(sub_l q subl)
+                            end
+                  end
